b1c2e15838e7d4ed56cc0c13ad4045bc3e083f1b
Tyler Hancock <darklime@live.ca>
2021-01-25 03:21:04 -0700

    Added banner patterns for coin piles.

--- src/main/java/net/darkhax/coins/Coins.java
+++ src/main/java/net/darkhax/coins/Coins.java
@@ -5,9 +5,13 @@ import org.apache.logging.log4j.Logger;
 
 import net.darkhax.bookshelf.item.ItemGroupBase;
 import net.darkhax.bookshelf.registry.RegistryHelper;
+import net.minecraft.item.BannerPatternItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.Properties;
+import net.minecraft.tileentity.BannerPattern;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Rarity;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -33,12 +37,28 @@ public class Coins {
         this.createSimpleItem("coin_pile_platinum");
         this.createSimpleItem("coin_pile_diamond");
         
+        registerItemPattern("pile_copper", Rarity.UNCOMMON);
+        registerItemPattern("pile_iron", Rarity.UNCOMMON);
+        registerItemPattern("pile_gold", Rarity.UNCOMMON);
+        registerItemPattern("pile_platinum", Rarity.UNCOMMON);
+        registerItemPattern("pile_diamond", Rarity.UNCOMMON);
+        registerItemPattern("pile_grayscale", Rarity.COMMON);
+        
         this.registry.initialize(FMLJavaModLoadingContext.get().getModEventBus());
     }
     
     private Item createSimpleItem (String id) {
         
         return this.registry.items.register(new Item(new Properties()), id);
+    }
+    
+    /**
+     * TODO Remove in 1.17
+     */
+    public void registerItemPattern (String id, Rarity rarity) {
+        
+        final BannerPattern pattern = registry.banners.registerPattern(id, true);
+        registry.items.register(new BannerPatternItem(pattern, new Item.Properties().maxStackSize(1).rarity(rarity)), id + "_banner_pattern");
     }
     
     private final ItemStack createTabIcon () {
